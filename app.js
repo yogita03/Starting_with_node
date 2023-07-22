9e1e359f2a301148df63b0c3d70f2a6b4e298060
yogita03 <patilyogita408@gmail.com>
2023-07-22 09:59:42 +0530

    request/response

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-    // console.log('Hello, my name is Yogita');
-    res.end(`Hello, my name is Yogita`);
+    console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+   
+    res.write('<head><title>My First Page</title></head>');
+    if(req.url === '/home'){
+        res.write('<body><h1>Welcome home</h1></body>');
+       }
+
+       else if(req.url === '/about')
+       {
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+       }
+
+       else if(req.url === '/node'){
+        res.write('<body><h1>Welcome to my Node Js project.</h1></body>');
+    }
+       
+    // res.write('<body><h1>Welcome to my Node Js project.</h1></body>');
+    res.write('</html>');
+    res.end();
 
 });
-server.listen(4000);
+server.listen(3000);
 
 
